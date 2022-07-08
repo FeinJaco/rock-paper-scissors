701ddb82b9d6a33b0af47ad456297b7212aae360
FeinJaco <jfeinla1@binghamton.edu>
2022-07-08 15:17:48 -0400

    Abstract functions into generating results and reading results, and add enums

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,58 +1,84 @@
-console.log(game());
+console.log(playAndReadGame());
+
+const Options = {
+    Rock: 0,
+    Paper: 1,
+    Scissors: 2
+}
+const Outcomes = {
+    Win: 0,
+    Lose: 1,
+    Tie: 2
+}
+Object.freeze(Options);
+Object.freeze(Outcomes);
+const optionNames = ["Rock", "Paper", "Scissors"];
 
 function computerPlay() {
-    switch (Math.floor(3*Math.random())) {
-        case 0:
-            return "Rock";
-        case 1:
-            return "Paper";
-        case 2:
-            return "Scissors"
-    }
+    return Math.floor(3*Math.random());
 }
 
 function playerPlay() {
     while (true) {
-        switch (prompt("Type 'Rock', 'Paper', or 'Scissors'").toUpperCase()) {
-            case 'ROCK':
-                return "Rock";
-            case 'PAPER':
-                return "Paper";
-            case 'SCISSORS':
-                return "Scissors";
-            default:
-                console.log("Invalid selection");
+        let selection = prompt("Type 'Rock', 'Paper', or 'Scissors'");
+        if (selection)
+            selection = selection.toUpperCase();
+        else
+            throw new Error("Cancelled game");
+        for (let i = 0; i < 3; i++) {
+            if (selection == optionNames[i].toUpperCase())
+                return i;
         }
+        console.log("Invalid selection");
     }
 }
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == computerSelection)
-        return "It's a tie!";
-    if ((playerSelection == "Rock" && computerSelection == "Paper") 
-    || (playerSelection == "Paper" && computerSelection == "Scissors") 
-    || (playerSelection == "Scissors" && computerSelection == "Rock"))
-        return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    return `You Win! ${playerSelection} beats ${computerSelection}`;
+        return Outcomes.Tie;
+    if ((playerSelection == Options.Rock && computerSelection == Options.Paper) 
+    || (playerSelection == Options.Paper && computerSelection == Options.Scissors) 
+    || (playerSelection == Options.Scissors && computerSelection == Options.Rock))
+        return Outcomes.Lose;
+    return Outcomes.Win;
 }
 
-function game() {
-    console.log("Welcome to a best of 5 game of Rock Paper Scissors!");
-    let playerScore = 0;
-    let computerScore = 0;
-    for (let i = 0; i < 5; i++) {
-        let message = playRound(playerPlay(), computerPlay());
-        console.log(message);
-        if (message.substring(4,7) == "Win")
-            playerScore++;
-        else if (message.substring(4,8) == "Lose")
-            computerScore++;
+function readRoundWinner(outcome, playerChoice, computerChoice) {
+    switch(outcome) {
+        case Outcomes.Win:
+            `You Win! ${playerChoice} beats ${computerChoice}`;
+        case Outcomes.Lose:
+            return `You Lose! ${computerChoice} beats ${playerChoice}`;
+        case Outcomes.Tie:
+            return "It's a tie!";
     }
-    let result;
+}
+
+function game(scoreCard) {
+    console.log("Welcome to a best of 5 game of Rock Paper Scissors!");
+    for (let i = 0; i < 3; i++)
+        scoreCard[i] = 0;
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = playerPlay();
+        let computerSelection = computerPlay();
+        let outcome = playRound(playerSelection, computerSelection);
+        console.log(readRoundWinner(outcome, optionNames[playerSelection], optionNames[computerSelection]));
+        scoreCard[outcome]++;
+    }
+    return scoreCard;
+}
+
+function readGameWinner(scoreCard) {
+    playerScore, computerScore, tieScore = game(scoreCard);
     if (playerScore == computerScore)
         result = `It's a tie!`;
     if (playerScore > computerScore)
         result = `You Win!`;
     result = `You Lose!`;
-    return result + ` Score: ${playerScore}-${computerScore}-${5 - playerScore - computerScore}`
+    return result + ` Score: ${playerScore}-${computerScore}-${tieScore}`;
+}
+
+function playAndReadGame() {
+    let scoreCard = [0,0,0];
+    readGameWinner(game(scoreCard));
 }
