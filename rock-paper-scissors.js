fd3fbe61c0f70b910c0089373871976b89e74981
FeinJaco <jfeinla1@binghamton.edu>
2022-07-08 14:11:57 -0400

    Implement round of player vs computer play

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,14 +1,37 @@
-console.log(computerPlay());
+console.log(playRound(playerPlay(), computerPlay()));
 
 function computerPlay() {
     switch (Math.floor(3*Math.random())) {
         case 0:
             return "Rock";
-            break;
         case 1:
             return "Paper";
-            break;
         case 2:
             return "Scissors"
     }
+}
+
+function playerPlay() {
+    while (true) {
+        switch (prompt("Type 'Rock', 'Paper', or 'Scissors'").toUpperCase()) {
+            case 'ROCK':
+                return "Rock";
+            case 'PAPER':
+                return "Paper";
+            case 'SCISSORS':
+                return "Scissors";
+            default:
+                alert("Invalid selection");
+        }
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection)
+        return "It's a tie!";
+    if ((playerSelection == "Rock" && computerSelection == "Paper") 
+    || (playerSelection == "Paper" && computerSelection == "Scissors") 
+    || (playerSelection == "Scissors" && computerSelection == "Rock"))
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    return `You Win! ${playerSelection} beats ${computerSelection}`;
 }
